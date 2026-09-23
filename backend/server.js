f5ac40ed2c0e0d3050f5c07d6562d382e6601762
gautam thakur <mnrhag5@gmail.com>
2026-09-23 18:12:01 +0530

    fix server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ let connectDB = require('./src/db/db')
 
 connectDB()
 
-app.listen(3000,()=>{
-    console.log('Server is running on port : 3000')
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Server is running on port : ' + (process.env.PORT || 3000))
 })
-
-
